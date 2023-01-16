22394bda43144e167e0ab7c5d19642ce5d0d5bc8
Mathieu GRUSON <mathieu.gruson@gmail.com>
2023-01-16 11:11:13 +0100

    setup completed

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,19 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 10:46:25 by mgruson           #+#    #+#             */
-/*   Updated: 2023/01/16 10:48:02 by mgruson          ###   ########.fr       */
+/*   Updated: 2023/01/16 11:10:03 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cub3d.h"
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	(void)mlx_win;
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	mlx_loop(mlx);
+}
--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,21 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 10:54:34 by mgruson           #+#    #+#             */
-/*   Updated: 2023/01/16 10:54:35 by mgruson          ###   ########.fr       */
+/*   Updated: 2023/01/16 11:04:30 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <fcntl.h>
+# include <errno.h>
+# include <stdio.h>
+# include "libs/libft/includes/libft.h"
+# include "libs/libft/includes/ft_printf.h"
+# include "libs/libft/includes/get_next_line.h"
+# include "libs/minilibx-linux/mlx.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+
+#endif
