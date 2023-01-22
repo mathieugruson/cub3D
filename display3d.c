fdaf0318e051d2ed304dae870ded868dc5610ffd
Mathieu GRUSON <mathieu.gruson@gmail.com>
2023-01-22 19:01:07 +0100

    resolve fish eye + ray lenght

--- display3d.c
+++ display3d.c
@@ -6,7 +6,7 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 16:10:11 by chillion          #+#    #+#             */
-/*   Updated: 2023/01/22 14:34:42 by mgruson          ###   ########.fr       */
+/*   Updated: 2023/01/22 18:56:10 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void ft_draw_line_dir3d(t_v *v, int y, int x, double degree, double i, double *tab)
 {
-	(void)y;
 	double pi = 3.1415926535897932384626433832;
 	double resultx = find_end_x(degree + i); //arrondi
 	double resulty = find_end_y(degree + i); //arrondi
@@ -26,13 +25,14 @@ void ft_draw_line_dir3d(t_v *v, int y, int x, double degree, double i, double *t
 	double view = 30 - i; ;
 	if (view < 0)
 		view *= -1;
+	// printf("view = %f\n", view);
 	while (1)
 	{
 		if (v->m.map[(int)pixely / XSIZE][(int)pixelx / XSIZE] == '1')
 		{
-			*tab = ((y - pixely) * sin((90) * pi / 180) / sin((180 - 90 - (view)) * pi / 180));	// taille avec fish eye
-			// il faut peut etre stocker pixely du centre. comment faire ? lancer un rayon a angle 0 et le garder
+			*tab = sqrt((((double)x - pixelx) * ((double)x - pixelx)) + (((double)y - pixely) * ((double)y - pixely)));
 			*tab = *tab * cos((view) * pi / 180);  // correction fish eye
+			*tab = (64 / *tab * 277); // a l echelle
 			return ;
 		}
 		ft_my_mlx_pixel_put(&v->ig, pixely, pixelx, ft_rgb_to_int(0, 50, 150, 250));
@@ -55,14 +55,14 @@ void	ft_draw_line_circle3d(t_v *v, int y, int x)
 		left = 360 + left;
 	if (right > 360)
 		right = right - 360;
-	/* METRIC 1/ */
-	printf("degree : %i\n", v->m.degree);
+	/* METRIC 1/2 */
+	// printf("degree : %i\n", v->m.degree);
 	/**/
 	while(index <= 320)
 	{
-		/* METRICS  2/*/
-		printf("index : %i\n", index);
-		printf("left : %f, i %f, left + i : %f\n", left, i, (left + i));
+		/* METRICS  2/2*/
+		// printf("index : %i\n", index);
+		// printf("left : %f, i %f, left + i : %f\n", left, i, (left + i));
 		/**/
 		ft_draw_line_dir3d(v, y, x, left,  i, &tab[index]);
 		i = i + 0.1875;
@@ -74,10 +74,20 @@ void	ft_draw_line_circle3d(t_v *v, int y, int x)
 	index = 0;
 	while(index <= 320)
 	{
-		tab[index] = ((double)(64 / tab[index] * 277)); // a l echelle
+		/* METRIC 1/1*/
+		// printf("index : %i\n", index);
+		// printf("column size avant mise a l echelle: %f\n", tab[index]);
+		/**/
+		/* METRIC 2/2*/
+		// printf("column size apres mise a l'echelle : %f\n", tab[index]);
+		/**/
 		while(z <= tab[index])
 		{
-			ft_my_mlx_pixel_put(&v->ig2, ((tab[index] / 2) + z), (index + 2), ft_rgb_to_int(0, 255, 0, 255));
+			/* METRIC 3/3*/
+			// printf(" z : %i, tab[index] %f\n", z, tab[index]);
+			// printf(" on screen : y = %i, x = %f\n", (index), ((tab[index] / 2) + z));
+			/**/
+			ft_my_mlx_pixel_put(&v->ig2, ((tab[index] / 2) + z), (index), ft_rgb_to_int(0, 255, 0, 255));
 			z++;
 		}
 		z = 0;
